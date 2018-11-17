bf26f5a1c7fdff381f788bc68b316d473ed699c6
fbriggs4 <fred.briggs@vektortek.com>
2018-11-17 08:33:06 -0500

    Add files via upload

---
+++ ESPblinky.ino
@@ -0,0 +1,19 @@
+int led = 2;  //built in LED on pins 2 and 16
+
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(led, OUTPUT); //define the pin as an input
+  
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  digitalWrite(led, HIGH);
+  delay(500);
+  digitalWrite(led, LOW);
+  delay(500);
+  
+
+}
