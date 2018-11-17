541d0c06a76637dc79dab0e1b7c5d058ddd98698
fbriggs4 <fred.briggs@vektortek.com>
2018-11-17 08:23:44 -0500

    Add files via upload

---
+++ buttonPress.ino
@@ -0,0 +1,28 @@
+
+
+int door = 16; //top right labeled D0
+int led = 2; // second LED is on GPIO2
+
+int sensor;
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  pinMode(door, INPUT);  //set the pin to be an input so we can read it
+  digitalWrite(door, LOW); //set it to low (i.e. o volts)
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+  sensor = digitalRead(door);  //read the state of the pin
+  Serial.print("the door reading = ");  //print the state to the serial monitor
+  Serial.println(sensor);
+  delay(500);  //wait a little so we dont go to fast for the serial
+ 
+  
+
+
+  
+  
+}
