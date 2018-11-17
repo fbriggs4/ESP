851d9a712a4696455bd61ea114c3272fafa063ee
fbriggs4 <fred.briggs@vektortek.com>
2018-11-17 08:23:26 -0500

    Add files via upload

---
+++ analogReadESP.ino
@@ -0,0 +1,19 @@
+int pot = A0;  //pin used for reading the 10k pot
+int input; //stores the analog reading from the pot
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  pinMode(pot, INPUT); //define the pin as an input
+  digitalWrite(pot, LOW);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  input = analogRead(pot);
+  Serial.print("alalog reading = ");
+  Serial.println(input);
+  
+
+}
